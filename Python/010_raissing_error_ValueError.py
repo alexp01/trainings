a2a96ba92adabb5430fea40acc5005aa3ae09f5a
alexp01 <34630182+alexp01@users.noreply.github.com>
2020-06-19 10:40:47 +0200

    .
    .

---
+++ Python/010_raissing_error_ValueError.py
@@ -0,0 +1,7 @@
+def count_from_0_to_n(n):
+    if n < 0:
+        raise ValueError('You provided a negative int, please give a number above 0')
+    for x in range(0,n+1):
+        print (x)
+
+count_from_0_to_n(-7)

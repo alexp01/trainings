6f6f98d17fb3ec4149c5f53021948a201aa3f65c
alexp01 <34630182+alexp01@users.noreply.github.com>
2020-06-25 13:21:13 +0200

    .
    .

--- Python/2_Files_management/202_writing_form_another_file/app.py
+++ Python/2_Files_management/202_writing_form_another_file/app.py
@@ -2,31 +2,29 @@
 # https://www.udemy.com/course/the-complete-python-course/learn/lecture/9445282#questions
 
 file1 = open('friends.txt', 'r')
-file1_content = file1.readlines()
-
-for x in range(0, len(file1_content)-1):
-    file1_content[x] = file1_content[x][:-1]
-
-name1 = input ('please give the name of the first friend:_')
-name2 = input ('please give the name of the second friend:_')
-name3 = input ('please give the name of the third friend:_')
+file1_content = [line.strip() for line in file1.readlines()]
+# variable.strip( will eliminate any spaces or \n from a string. before and after that string)
+# Its easier to use strip that doing it like i did bellow
+#for x in range(0, len(file1_content)-1):
+#    file1_content[x] = file1_content[x][:-1]
+file1_content_set = set(file1_content)
+name = input ('please give 3 names separated by a comma:_').split(',')
+name_set = set(name)
+file1.close()
 
 file2 = open('close_friends.txt', 'w')
-
-if name1 in file1_content:
-    file2. write("\n" + name1 + "\n")
-if name2 in file1_content:
-    file2. write(name2 + "\n")
-if name3 in file1_content:
-    file2. write(name3 + "\n")
-
+# Its better to use sets and intersection function, than the below code.
+#for x in range(0, len(name)):
+#    if name[x] in file1_content:
+#        file2.write("\n" + name[x])
+# On the other hand you still have to use a for to add each Set element into the file. So you will use a for in the end
+intersection_set = name_set.intersection(file1_content_set)
+for x in intersection_set:
+    print(f"We've found a friend : {x}")
+    file2.write(x + '\n')
 file2.close()
 
 file2 = open('close_friends.txt', 'r')
 file2_content = file2.read()
-
-print (file1_content)
-print (f'All known friens were added to the close_frinds.txt file. Content: {file2_content}')
-
-file1.close()
+print (f'All known friens were added to the close_frinds.txt file. Content:\n{file2_content}')
 file2.close()

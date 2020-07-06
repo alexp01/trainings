badfc18f34a3447e285bfccb251d472b9a8b9036
alexp01 <34630182+alexp01@users.noreply.github.com>
2020-07-06 14:14:55 +0200

    .
    .

---
+++ Python/5_Advance_buily_in_functions/501_generator_example/app.py
@@ -0,0 +1,18 @@
+
+# https://www.udemy.com/course/the-complete-python-course/learn/lecture/9445596#questions
+
+# yield can be used to temporary stop a function, so that you can coninue it afterwards 
+
+def get_100_numbers() -> int:
+    i = 0
+    while i<100:
+        yield i
+        i +=1
+
+# yield is like a return, but it will also remember inside the function the last execution point and the values
+# so when it reaches yield it will return i, and when its called again by next(variable), it will continue with i = I=1, and then run the while again
+
+x = get_100_numbers()
+print (x)
+print (next(x)) # this will call again the function and it will continue from where it was stopped -> when i = 0
+print (next(x)) # this will call again the function and it will continue from where it was stopped -> when i = 1

8522c51677e128e2deeaaa5b48056fa5de1930d9
alexp01 <34630182+alexp01@users.noreply.github.com>
2020-07-06 16:53:32 +0200

    .
    .

---
+++ Python/5_Advance_buily_in_functions/502_Iterator_class_example/app.py
@@ -0,0 +1,38 @@
+
+# https://www.udemy.com/course/the-complete-python-course/learn/lecture/9445604#questions
+
+class Generate100Numbers: # This is called a Iterator class, and its behaving identically as a Generator
+    def __init__(self):
+        self.number = 0
+
+    def __next__(self):
+        if self.number < 100:
+            current = self.number
+            self.number += 1
+            return current
+        else:
+            raise StopIteration()
+
+my_gen = Generate100Numbers()
+
+print(my_gen.number)
+print(next(my_gen)) # this prints 0 as we first return the number and then we increment it
+# print (my_gen.__next__()) # this is identical to : print(next(my_gen))
+print(next(my_gen))
+print(next(my_gen))
+
+# Not all Iterators are generators, as your next method can return an element from a list, for example and iterate through its elements
+# Example:
+
+class Give5valuesFromList: # this is also an Iterator but not an generator
+    def __init__(self):
+        self.list = [0, 1, 2, 3, 4]
+        self.i = 0
+
+    def __next__(self):
+        if self.i < len(self.list):
+            current = self.list[i]
+            self.i += 1
+            return current
+        else:
+            raise StopIteration()
